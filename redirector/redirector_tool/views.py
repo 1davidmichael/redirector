74cf5e35c68e4c6d0b5ce2a0ff9b34b05572fd09
David Michael <david.michael@meredith.com>
2014-07-29 10:41:15 -0500

    more goodness

--- redirector/redirector_tool/views.py
+++ redirector/redirector_tool/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+
+def detail(request, redirect_id):
+    return HttpResponse("You are looking at redirect %s." % redirect_id)
