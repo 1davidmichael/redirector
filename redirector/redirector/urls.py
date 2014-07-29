1b8ccd6d84e875f0c279d2ca1c7ddc34cae0e8c6
David Michael <david.michael@meredith.com>
2014-07-29 10:15:00 -0500

    adding basic hello world for redirects view, adding more later

--- redirector/redirector/urls.py
+++ redirector/redirector/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'redirector.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^redirects/', include('redirector_tool.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
