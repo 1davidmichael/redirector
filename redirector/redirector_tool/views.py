dd7e79af4f13cbc044bb00db82ff57832089fd9f
David Michael <david.michael@meredith.com>
2014-07-29 11:59:40 -0500

    adding in more

--- redirector/redirector_tool/views.py
+++ redirector/redirector_tool/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from redirector_tool.models import Redirect
 # Create your views here.
 def index(request):
     all_redirects = Redirect.objects.order_by('-expiration_date')
-    output = ', '.join([r.source_url for r in all_redirects])
-    return HttpResponse(output)
+    context = {'all_redirects': all_redirects}
+    return render(request, 'redirector_tool/index.html', context)
 
 def detail(request, redirect_id):
-    return HttpResponse("you're looking at redirect %s" % redirect_id)
+    redirect = get_object_or_404(Redirect, pk=redirect_id)
+    return render(request, 'redirector_tool/detail.html', {'redirect': redirect})
