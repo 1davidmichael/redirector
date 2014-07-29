e73ee17f6cbfb8bde0b1816d05c49de3bb344813
David Michael <david.michael@meredith.com>
2014-07-29 11:16:01 -0500

    starting views

--- redirector/redirector_tool/views.py
+++ redirector/redirector_tool/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from redirector_tool.models import Redirect
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    all_redirects = Redirect.objects.order_by('-expiration_date')
+    output = ', '.join([r.source_url for r in all_redirects])
+    return HttpResponse(output)
 
 def detail(request, redirect_id):
-    return HttpResponse("You are looking at redirect %s." % redirect_id)
+    return HttpResponse("you're looking at redirect %s" % redirect_id)
